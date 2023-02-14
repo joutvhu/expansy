575175490a9c4a6a71237a45b84b40f68ab5ea9a
Giao Ho <joutvhu@gmail.com>
2023-02-15 01:04:05 +0700

    Design

--- src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/NumericMatcher.java
+++ src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/NumericMatcher.java
@@ -3,6 +3,7 @@ package com.joutvhu.dynamic.expression.analysis.match.func;
 import com.joutvhu.dynamic.expression.analysis.match.filter.LinearFilter;
 import com.joutvhu.dynamic.expression.analysis.match.MatchFunction;
 import com.joutvhu.dynamic.expression.analysis.match.Matcher;
+import com.joutvhu.dynamic.expression.analysis.match.filter.StopPoint;
 
 public class NumericMatcher<E> extends MatchFunction<E> {
     public NumericMatcher(Matcher<E> parent) {
@@ -15,6 +16,37 @@ public class NumericMatcher<E> extends MatchFunction<E> {
 
     @Override
     public void match(LinearFilter filter) {
-
+        boolean started = false;
+        boolean decimal = false;
+        boolean negative = false;
+        while (true) {
+            StopPoint point = filter.next();
+            if (point == null) break;
+            if (point.getCharacter() == '-') {
+                if (negative || started) {
+                    filter.error("");
+                    break;
+                }
+                negative = true;
+                continue;
+            }
+            if ('0' <= point.getCharacter() && point.getCharacter() <= '9') {
+                started = true;
+                filter.push();
+                continue;
+            }
+            if (point.getCharacter() == '.') {
+                if (!started) {
+                    filter.error("");
+                    break;
+                }
+                if (decimal) {
+                    filter.error("");
+                    break;
+                }
+                decimal = true;
+                continue;
+            }
+        }
     }
 }
