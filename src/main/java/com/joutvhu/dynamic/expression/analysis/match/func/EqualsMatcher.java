891e80d31264b19d3975110931ec152a84105406
Giao Ho <joutvhu@gmail.com>
2023-02-15 01:42:07 +0700

    Design

--- src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/EqualsMatcher.java
+++ src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/EqualsMatcher.java
@@ -4,23 +4,32 @@ import com.joutvhu.dynamic.expression.analysis.match.MatchFunction;
 import com.joutvhu.dynamic.expression.analysis.match.Matcher;
 import com.joutvhu.dynamic.expression.analysis.match.filter.LinearFilter;
 import com.joutvhu.dynamic.expression.analysis.match.filter.StopPoint;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EqualsMatcher<E> extends MatchFunction<E> {
     private List<String> values;
 
     public EqualsMatcher(Matcher<E> parent, String value) {
         super(parent);
-        this.values = Arrays.asList(value);
+        if (value == null || value.length() == 0)
+            throw new IllegalArgumentException("");
+        this.values = List.of(value);
     }
 
     public EqualsMatcher(Matcher<E> parent, List<String> values) {
         super(parent);
-        this.values = values;
-        values.sort(Comparator.comparingInt(String::length));
+        if (values == null || values.isEmpty())
+            throw new IllegalArgumentException("");
+        this.values = values.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .filter(StringUtils::isNotEmpty)
+                .collect(Collectors.toList());
+        if (this.values.isEmpty())
+            throw new IllegalArgumentException("");
     }
 
     @Override
