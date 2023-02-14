f50dc736702ce4ec89d178a5b42b5f485556b01c
Giao Ho <joutvhu@gmail.com>
2023-02-15 01:14:42 +0700

    Design

--- src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/EqualsMatcher.java
+++ src/main/java/com/joutvhu/dynamic/expression/analysis/match/func/EqualsMatcher.java
@@ -1,10 +1,12 @@
 package com.joutvhu.dynamic.expression.analysis.match.func;
 
-import com.joutvhu.dynamic.expression.analysis.match.filter.LinearFilter;
 import com.joutvhu.dynamic.expression.analysis.match.MatchFunction;
 import com.joutvhu.dynamic.expression.analysis.match.Matcher;
+import com.joutvhu.dynamic.expression.analysis.match.filter.LinearFilter;
+import com.joutvhu.dynamic.expression.analysis.match.filter.StopPoint;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class EqualsMatcher<E> extends MatchFunction<E> {
@@ -18,10 +20,24 @@ public class EqualsMatcher<E> extends MatchFunction<E> {
     public EqualsMatcher(Matcher<E> parent, List<String> values) {
         super(parent);
         this.values = values;
+        values.sort(Comparator.comparingInt(String::length));
     }
 
     @Override
     public void match(LinearFilter filter) {
-
+        int len = 0;
+        StopPoint point = null;
+        for (String value : values) {
+            if (len != value.length())
+                point = filter.next(value.length() - len);
+            if (point == null) break;
+            len = point.getValue().length();
+            if (len != value.length()) {
+                filter.error("");
+                break;
+            }
+            if (value.equals(point.getValue()))
+                filter.push();
+        }
     }
 }
