10607e1757d91b16a4302f016f8e8bc7bbf7e38d
Giao Ho <joutvhu@gmail.com>
2023-02-16 09:00:13 +0700

    LICENSE

--- src/main/java/com/joutvhu/expansy/match/definer/DefinerUtil.java
+++ src/main/java/com/joutvhu/expansy/match/definer/DefinerUtil.java
@@ -1,9 +1,11 @@
 package com.joutvhu.expansy.match.definer;
 
 import com.joutvhu.expansy.element.Element;
+import com.joutvhu.expansy.match.Definer;
 import com.joutvhu.expansy.match.Matcher;
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @UtilityClass
@@ -12,5 +14,11 @@ public class DefinerUtil {
         DefaultDefiner<E> definer = new DefaultDefiner<>();
         element.define(definer);
         return definer.matchers;
+    }
+
+    public <E> List<Matcher<E>> matchersOf(Definer<E> definer) {
+        if (definer instanceof DefaultDefiner)
+            return ((DefaultDefiner<E>) definer).matchers;
+        return new ArrayList<>();
     }
 }
