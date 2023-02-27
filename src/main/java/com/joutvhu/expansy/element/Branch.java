3f74a54c5cbeb95c9b121184dc7c339a8cb0380f
Giao Ho <joutvhu@gmail.com>
2023-02-28 01:27:46 +0700

    Add comment

--- src/main/java/com/joutvhu/expansy/element/Branch.java
+++ src/main/java/com/joutvhu/expansy/element/Branch.java
@@ -29,6 +29,9 @@ public class Branch<E> extends ArrayList<Node<E>> {
         return results;
     }
 
+    /**
+     * Highlight an Element being checked at the specified position.
+     */
     public void start(int index, Element<E> element) {
         Set<Element<E>> elements = checking.get(index);
         if (elements == null) {
@@ -38,6 +41,9 @@ public class Branch<E> extends ArrayList<Node<E>> {
         elements.add(element);
     }
 
+    /**
+     * Checks if an element is checking at the specified position.
+     */
     public boolean started(int index, Element<E> element) {
         Set<Element<E>> elements = checking.get(index);
         if (elements == null)
@@ -45,32 +51,59 @@ public class Branch<E> extends ArrayList<Node<E>> {
         return elements.contains(element);
     }
 
+    /**
+     *
+     */
     public void complete(int index, Element<E> element) {
         Set<Element<E>> elements = checking.get(index);
         if (elements != null)
             elements.remove(element);
     }
 
+    /**
+     * Set a value with key into the shared state of this branch.
+     */
     public <P> void set(String key, P value) {
         shared.put(key, value);
     }
 
+    /**
+     * Get a value with key from the shared state of this branch.
+     */
     public <P> P get(String key) {
         return (P) shared.get(key);
     }
 
+    /**
+     * Returns the first node in this branch.
+     */
     public Node<E> first() {
         int len = size();
         return len > 0 ? get(0) : null;
     }
 
+    /**
+     * Returns the node at the specified position in this branch.
+     */
+    public Node<E> at(int index) {
+        return get(index);
+    }
+
+    /**
+     * Returns the last node in this branch.
+     */
     public Node<E> last() {
         int len = size();
         return len > 0 ? get(len - 1) : null;
     }
 
+    /**
+     * Replace a node at the specified position with a new node
+     */
     public void replace(int index, Node<E> node) {
         boolean shouldRemove = true;
+        if (index < 0)
+            index = 0;
         if (index >= size()) {
             index = size();
             shouldRemove = false;
@@ -80,14 +113,23 @@ public class Branch<E> extends ArrayList<Node<E>> {
             remove(index + 1);
     }
 
+    /**
+     * Replace first node with a new node
+     */
     public void replaceFirst(Node<E> node) {
         replace(0, node);
     }
 
+    /**
+     * Replace last node with a new node
+     */
     public void replaceLast(Node<E> node) {
         replace(size() - 1, node);
     }
 
+    /**
+     * Returns a copy of this branch.
+     */
     @Override
     public Branch<E> clone() {
         Branch<E> branch = new Branch<>();
--- src/main/java/com/joutvhu/expansy/element/Element.java
+++ src/main/java/com/joutvhu/expansy/element/Element.java
@@ -3,11 +3,20 @@ package com.joutvhu.expansy.element;
 import com.joutvhu.expansy.match.Definer;
 
 public abstract class Element<E> {
+    /**
+     * Get name of the element, default is class name.
+     */
     public String name() {
         return this.getClass().getSimpleName();
     }
 
+    /**
+     * Define the structure of the element
+     */
     public abstract void define(Definer<E> definer);
 
+    /**
+     * Render the element, create executable component.
+     */
     public abstract E render(Node<E> node);
 }
